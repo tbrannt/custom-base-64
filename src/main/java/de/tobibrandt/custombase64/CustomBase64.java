73fb704e7241255a9049afd9f10cfbad806d5a55
tbrannt <brandt.tob@gmail.com>
2015-05-07 11:02:36 +0200

    comment formatting

--- src/main/java/de/tobibrandt/custombase64/CustomBase64.java
+++ src/main/java/de/tobibrandt/custombase64/CustomBase64.java
@@ -19,9 +19,9 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
 /**
- * This class provides the functionality to encode/decode a byte array to/from a Base64 String.<br>
- * This Base64 class does NOT follow the RFC 3548 or RFC 4648 specification of Base64 as the used charset can be specified and no padding characters are 
- * used.<br>
+ * This CustomBase64 class provides the functionality to encode/decode a byte array to/from a Base64 String.<br>
+ * This CustomBase64 class does NOT follow the RFC 3548 or RFC 4648 specification of Base64 as the used charset can be specified 
+ * and no padding characters are used.<br>
  * So only the characters provided by the user by calling the {@link #setBase64Charset(String)} will appear in the encoded String and no other characters 
  * like equal signs '=' (sometimes used for padding in Base64) will appear.
  * 
